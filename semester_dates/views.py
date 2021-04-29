42fd103ce2c40280c40d51b42dc0224b4c2f6ac9
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2021-04-29 12:05:08 +0400

    dates are added after visiting directory page. BUG fixed

--- semester_dates/views.py
+++ semester_dates/views.py
@@ -6,15 +6,6 @@ from .models import dates
 
 @login_required
 def semester_dates(request):
-	if len(dates.objects.filter(user=request.user.id)) == 0:
-		dates.objects.create(
-			user=request.user,
-			first_semester_start='2020-09-15',
-			first_semester_end='2020-12-31',
-			second_semester_start='2021-01-15',
-			second_semester_end='2021-07-01'
-			)
-
 	userDates = dates.objects.get(user=request.user.id)
 	DisplayDates = dates.objects.filter(user=request.user.id)
 	
