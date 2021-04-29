ef40566b085f21751a883a12df00a85cb58c4b4e
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2021-04-29 15:20:15 +0400

    added commands in settings.py

--- Marks/settings.py
+++ Marks/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm)g&gv*5iw*eq0fi48=5nv#hjn$eey^ujpyx_6y^u2ryfbtebnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'schoolmarks.herokuapp.com']
 
@@ -136,9 +136,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
+    #BASE_DIR / "static",
     #'/var/www/static/',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
