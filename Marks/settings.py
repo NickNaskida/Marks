0160b40efbfcb2e900e74ca58e67313caca37830
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2021-04-30 17:56:15 +0400

    register server error

--- Marks/settings.py
+++ Marks/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm)g&gv*5iw*eq0fi48=5nv#hjn$eey^ujpyx_6y^u2ryfbtebnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'schoolmarks.herokuapp.com']
