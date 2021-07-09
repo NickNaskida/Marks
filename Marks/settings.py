cbc3d3ede96051f4fc07424bf163816135208941
Nikoloz Naskidashvili <82929931+Naskida15@users.noreply.github.com>
2021-07-09 11:11:39 +0400

    Update settings.py

--- Marks/settings.py
+++ Marks/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm)g&gv*5iw*eq0fi48=5nv#hjn$eey^ujpyx_6y^u2ryfbtebnk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
